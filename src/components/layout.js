8b398fbf96e53d9fc58065455869d64f92537c1f
Harsh Shah <harsh.120410116038@gmail.com>
2020-03-09 10:36:34 +0530

    Made Layout component stateful

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,54 +5,51 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useEffect } from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import Header from "./header"
-import Footer from "./footer"
+import React, { Component } from "react";
+import { StaticQuery, graphql } from "gatsby";
 import { ParallaxProvider } from 'react-scroll-parallax';
+import Header from "./header";
+import Footer from "./footer";
 import { creatCursor } from '../assets/js/helper';
-
 import "./app.scss";
 
+export default class Layout extends Component {
 
-const Layout = ({ children }) => {
-
-  useEffect(() => {
+  componentDidMount() {
     creatCursor();
-  });
+  }
 
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        } 
-      }
-    }
-  `)
-
-  return (
-    <>
-      <ParallaxProvider>
-        <React.Fragment>
-          <div className="main">
-            <main className='routed'>
-              <Header siteTitle={data.site.siteMetadata.title} />
-              <div className="cursor"></div>
-              <div className="main-content">{children}</div>
-              <Footer />
-            </main>
-          </div>
-        </React.Fragment>
-      </ParallaxProvider>
-    </>
-  )
+  render() {
+    const { children } = this.props;
+    return (
+      <>
+        <StaticQuery
+          query={graphql`
+                  query SiteTitleQuery {
+                    site {
+                      siteMetadata {
+                        title
+                      } 
+                    }
+                  }
+                    `}
+          render={data => (
+            <ParallaxProvider>
+              <React.Fragment>
+                <div className="main">
+                  <main className='routed'>
+                    <Header siteTitle={data.site.siteMetadata.title} />
+                    <div className="cursor"></div>
+                    <div className="main-content">{children}</div>
+                    <Footer />
+                  </main>
+                </div>
+              </React.Fragment>
+            </ParallaxProvider>
+          )}
+        />
+      </>
+    )
+  }
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
 
